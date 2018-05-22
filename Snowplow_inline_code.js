0a359e4452c3322ba81f6097d7687e3861c46e44
ryan janes <ryan.janes@gov.bc.ca>
2018-05-22 11:16:08 -0700

    Clean up read mes and inline codes
    Just fixing up the readme's in the folders, removing the incorrect html example.  Also updated the main snowplow inline script to have it go to the snowplow mini.

--- Snowplow_inline_code.js
+++ Snowplow_inline_code.js
@@ -4,7 +4,8 @@
  p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)
  };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;
  n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","//dexqzvifihpcx.cloudfront.net/kFaRq7wlqn92HjDbMi45LjA.js","snowplow"));
-window.snowplow('newTracker', 'co', 'ca-bc-gov-main.collector.snplow.net', {
+var collector = 'spm.gov.bc.ca';
+ window.snowplow('newTracker','rt',collector, {
  platform: 'web',
  respectDoNotTrack: true,
  post: true,

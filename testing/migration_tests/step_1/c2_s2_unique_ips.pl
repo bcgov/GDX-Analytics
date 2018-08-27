621e5852cad4a54bae2907efdad2d76ffa082956
Walle <mark.walle@gov.bc.ca>
2018-08-27 15:58:31 -0700

    usage updates

--- testing/migration_tests/step_1/c2_s2_unique_ips.pl
+++ testing/migration_tests/step_1/c2_s2_unique_ips.pl
@@ -1,3 +1,7 @@
+# File:   c2_s2_unique_ips.sh
+# Usage:  $ perl ./c2_s2_unique_ips.pl *.log
+#         - run in the folder containing logs.
+
 # prints every unique IP address per line over all input SDC log files
 
 my %ips;

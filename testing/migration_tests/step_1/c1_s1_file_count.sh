c1c072c38262cf7efe4ed126dc93af736abb575b
Walle <mark.walle@gov.bc.ca>
2018-08-28 13:44:53 -0700

    added maxdepth 1 to find, dropped wc -l in favour of grep -c, and added more details to header

--- testing/migration_tests/step_1/c1_s1_file_count.sh
+++ testing/migration_tests/step_1/c1_s1_file_count.sh
@@ -3,7 +3,10 @@
 # Usage:  $ ./c1_s1_file_count.sh <path_to_logs>
 
 # This script will output counts of logs where the filename
-# contains "YYYY-MM" for all months over 2016 and 2017
+# contains "YYYY-MM" for all months over 2016 and 2017.
+# The "Total" logs will be all files with the ".log" extesnsion.
+# As a result, the Total may differ from the sum of YYYY-MM parts
+# if there were any other .log files in the path specified.
 
 path_to_logs="$1"
 
@@ -18,8 +21,8 @@ for ((year=16;year<=17;year++));
 do
   for ((month=1;month<=12;month++));
   do
-    echo -n "${MONTHS[$month]} $year: ";find $path_to_logs -type f -print | grep -c "20$year-`printf %02d $month`"
+    echo -n "${MONTHS[$month]} $year: ";find $path_to_logs -maxdepth 1 -type f -print | grep -c "20$year-`printf %02d $month`.*\.log"
   done
 done
 echo "==============="
-echo -n "Total: ";find $path_to_logs -type f -print | grep '.log' | wc -l
+echo -n "Total: ";find $path_to_logs -type f -print | grep -c '\.log'

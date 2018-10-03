8b77c4c070104d26680669436ac44800c1225b5a
ryrjanes <36681256+ryrjanes@users.noreply.github.com>
2018-10-03 10:03:00 -0700

    add - appid, forceSecureTracker. updated js url

--- examples/gov.bc.ca_js_tracker/gov_sp_script.js
+++ examples/gov.bc.ca_js_tracker/gov_sp_script.js
@@ -3,12 +3,14 @@
 ;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
  p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)
  };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;
- n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","//dexqzvifihpcx.cloudfront.net/kFaRq7wlqn92HjDbMi45LjA.js","snowplow"));
+ n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","https://sp-js.apps.gov.bc.ca/kFaRq7wlqn92HjDbMi45LjA.js","snowplow"));
 var collector = 'spm.gov.bc.ca';
  window.snowplow('newTracker','rt',collector, {
+	appId: "snowplow_gov", 
 	platform: 'web',
 	respectDoNotTrack: true,
 	post: true,
+	forceSecureTracker: true,
 	contexts: {
 		webPage: true,
 		performanceTiming: true

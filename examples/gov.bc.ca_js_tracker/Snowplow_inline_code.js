10e9a3510dd6a5997876159b3df1b84e2d921d8b
ryrjanes <36681256+ryrjanes@users.noreply.github.com>
2018-10-09 09:49:54 -0700

    adding appid

--- examples/gov.bc.ca_js_tracker/Snowplow_inline_code.js
+++ examples/gov.bc.ca_js_tracker/Snowplow_inline_code.js
@@ -6,7 +6,7 @@
  n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","https://sp-js.apps.gov.bc.ca/kFaRq7wlqn92HjDbMi45LjA.js  ","snowplow"));
 var collector = 'spm.gov.bc.ca';
  window.snowplow('newTracker','rt',collector, {
-  appId: "",
+  appId: "Snowplow_standalone",
   platform: 'web',
   respectDoNotTrack: true,
   post: true,

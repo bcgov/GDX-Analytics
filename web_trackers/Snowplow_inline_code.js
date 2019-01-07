da88327e1fb89585c2bd314b5395d323ecab25a6
danpollock <dan.pollock@gov.bc.ca>
2019-01-07 15:21:08 -0800

    Update Snowplow_inline_code.js

--- web_trackers/Snowplow_inline_code.js
+++ web_trackers/Snowplow_inline_code.js
@@ -1,5 +1,4 @@
-// <!-- Snowplow starts plowing -->
-//  <script type="text/javascript">
+// <!-- Snowplow starts plowing - v1.2.9.2 -->
 ;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
  p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)
  };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;
@@ -19,5 +18,4 @@ var collector = 'spm.gov.bc.ca';
  window.snowplow('enableActivityTracking', 30, 30); // Ping every 30 seconds after 30 seconds
  window.snowplow('enableLinkClickTracking');
  window.snowplow('trackPageView');
-//  </script>
 //  <!-- Snowplow stop plowing -->

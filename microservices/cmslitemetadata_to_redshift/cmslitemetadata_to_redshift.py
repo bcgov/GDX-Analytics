7a45d419c35845e9f17de52b2fea871f3204d57c
Dan Pollock <dan.pollock@gov.bc.ca>
2019-04-15 22:40:37 -0700

    add topics to metadata script

--- microservices/cmslitemetadata_to_redshift/cmslitemetadata_to_redshift.py
+++ microservices/cmslitemetadata_to_redshift/cmslitemetadata_to_redshift.py
@@ -300,17 +300,27 @@ query = """
                 WHEN TRIM(SPLIT_PART(cm.ancestor_nodes, '|', 3)) = '' AND cm_parent.page_type = 'BC Gov Theme' THEN cm.parent_node_id -- the page's parent is a sub-theme
                 WHEN TRIM(SPLIT_PART(cm.ancestor_nodes, '|', 3)) <> '' THEN TRIM(SPLIT_PART(cm.ancestor_nodes, '|', 3)) -- take the third entry. The first is always blank as the string has '|' on each end and the second is the theme
                 ELSE NULL
-            END AS subtheme_id
+            END AS subtheme_id,
+            CASE
+                WHEN cm.parent_node_id = 'CA4CBBBB070F043ACF7FB35FE3FD1081' THEN NULL -- this page IS a theme, not a sub-theme
+                WHEN cm.ancestor_nodes = '||' AND cm.page_type = 'BC Gov Theme' THEN NULL -- this page is a sub-theme
+                WHEN TRIM(SPLIT_PART(cm.ancestor_nodes, '|', 3)) = '' AND cm_parent.page_type = 'BC Gov Theme' AND cm.page_type = 'Topic' THEN cm.node_id -- the page's parent is a sub-theme and it is a topic page
+                WHEN TRIM(SPLIT_PART(cm.ancestor_nodes, '|', 4)) = '' AND cm_parent.page_type = 'Topic' THEN cm.parent_node_id -- the page's parent is a topic        
+                WHEN TRIM(SPLIT_PART(cm.ancestor_nodes, '|', 4)) <> '' THEN TRIM(SPLIT_PART(cm.ancestor_nodes, '|', 4)) -- take the fourth entry. The first is always blank as the string has '|' on each end and the second is the theme, third is sub-theme
+                ELSE NULL
+            END AS topic_id
             FROM cmslite.metadata AS cm
-            LEFT JOIN cmslite.metadata AS cm_parent ON cm_parent.page_type = 'BC Gov Theme' AND cm_parent.node_id = cm.parent_node_id
+            LEFT JOIN cmslite.metadata AS cm_parent ON cm_parent.node_id = cm.parent_node_id
         )
     SELECT
         ids.*,
         cm_theme.title AS theme,
-        cm_sub_theme.title AS subtheme
+        cm_sub_theme.title AS subtheme,
+        cm_topic.title AS topic
         FROM ids
         LEFT JOIN cmslite.metadata AS cm_theme ON cm_theme.node_id = theme_id
         LEFT JOIN cmslite.metadata AS cm_sub_theme ON cm_sub_theme.node_id = subtheme_id
+        LEFT JOIN cmslite.metadata AS cm_topic ON cm_topic.node_id = subtheme_id
     ;
     """
 

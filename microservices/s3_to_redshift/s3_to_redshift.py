590c638f53cd1b335096b2a5281f5f74c20f13c7
Mark Walle <mark.walle@gov.bc.ca>
2019-07-17 16:29:56 -0700

    build processing list first to hand truncate option better. Fix indenting.

--- microservices/s3_to_redshift/s3_to_redshift.py
+++ microservices/s3_to_redshift/s3_to_redshift.py
@@ -89,7 +89,7 @@ else:
 client = boto3.client('s3')  # low-level functional API
 resource = boto3.resource('s3')  # high-level object-oriented API
 my_bucket = resource.Bucket(bucket)  # subsitute this for your s3 bucket name.
-b_name = my_bucket.name
+bucket_name = my_bucket.name
 
 # Database connection string
 conn_string = "dbname='snowplow' host='snowplow-ca-bc-gov-main-redshi-resredsh\
@@ -109,173 +109,178 @@ def copy_query(dbtable, batchfile, log):
 COPY {0}\nFROM 's3://{1}/{2}'\n\
 CREDENTIALS 'aws_access_key_id={3};aws_secret_access_key={4}'\n\
 IGNOREHEADER AS 1 MAXERROR AS 0 DELIMITER '|' NULL AS '-' ESCAPE;\n
-""".format(dbtable, b_name, batchfile, aws_key, aws_secret_key)
+""".format(dbtable, bucket_name, batchfile, aws_key, aws_secret_key)
     return query
 
 
-# TODO if truncate=true then we should only bother to process the latest
-# uploaded file, otherwise we should process all files found here
-if truncate:
-    latest_object = None
-    for object_summary in my_bucket.objects.filter(Prefix=source + "/"
-                                                   + directory + "/"):
-        key = object_summary.key
-        if re.search(doc + '$', key):
-            # Check to see if the file has been processed already
-            filename = key[-(key[::-1].index('/')):]
-            goodfile = destination + "/good/" + key
-            badfile = destination + "/bad/" + key
-            try:
-                client.head_object(Bucket=bucket, Key=goodfile)
-            except ClientError:
-                pass  # this object does not exist under the good destination path
-            else:
-                logger.debug("{0} processed already. Skip.".format(filename))
-                continue
-            try:
-                client.head_object(Bucket=bucket, Key=badfile)
-            except ClientError:
-                pass # this object does not exist under the bad destination path
-            else:
-                logger.debug("{0} failed already. Skip.".format(filename))
-                continue
-            logger.debug("{0} not already processed.".format(filename))
-            if latest_object is None:
-                latest_object = object_summary
-                continue
-            if object_summary.last_modified > latest_object.modified
-                latest_object = object_summary
-            # obj = client.get_object(Bucket=bucket, Key=object_summary.key)
+def is_processed(object_summary):
+    # Check to see if the file has been processed already
+    key = object_summary.key
+    filename = key[key.rfind('/')+1:]  # get the filename (after the last '/')
+    goodfile = destination + "/good/" + key
+    badfile = destination + "/bad/" + key
+    try:
+        client.head_object(Bucket=bucket, Key=goodfile)
+    except ClientError:
+        pass  # this object does not exist under the good destination path
+    else:
+        logger.debug("{0} was processed as good already.".format(filename))
+        return True
+    try:
+        client.head_object(Bucket=bucket, Key=badfile)
+    except ClientError:
+        pass  # this object does not exist under the bad destination path
+    else:
+        logger.debug("{0} was processed as bad already.".format(filename))
+        return True
+    logger.debug("{0} has not been processed.".format(filename))
+    return False
 
+
+# This bucket scan will find unprocessed objects.
+# objects_to_process will contain zero or one objects if truncate=True;
+# objects_to_process will contain zero or more objects if truncate=False.
+objects_to_process = []
 for object_summary in my_bucket.objects.filter(Prefix=source + "/"
                                                + directory + "/"):
-    if re.search(doc + '$', object_summary.key):
-        logger.debug('{0}:{1}'.format(b_name, object_summary.key))
-
-        # Check to see if the file has been processed already
-        batchfile = destination + "/batch/" + object_summary.key
-        goodfile = destination + "/good/" + object_summary.key
-        badfile = destination + "/bad/" + object_summary.key
-        try:
-            client.head_object(Bucket=bucket, Key=goodfile)
-        except Exception as e:
-            True
-        else:
-            logger.debug("File processed already. Skip.")
-            continue
-        try:
-            client.head_object(Bucket=bucket, Key=badfile)
-        except Exception as e:
-            True
-        else:
-            logger.debug("File failed already. Skip.")
-            continue
-        logger.debug("File not already processed. Proceed.")
-        obj = client.get_object(Bucket=bucket, Key=object_summary.key)
-        body = obj['Body']
-        # Create an object to hold the data while parsing
-        csv_string = ''
-        # Perform regex pattern replacements according to config, if defined
-        if 'global_regex' in data:
-            linefeed= ''
-            parsed_list = []
-            if(data['global_regex']['string_repl']):
-                inline_pattern = data['global_regex']['string_repl']['pattern']
-                inline_replace = data['global_regex']['string_repl']['replace']
-            body_stringified = body.read()
-            for line in body_stringified.splitlines():
-                if(data['global_regex']['string_repl']):
-                    line = line.replace(inline_pattern, inline_replace)
-                for exp in data['global_regex']['regexs']:
-                    parsed_line, num_subs = re.subn(
-                        exp['pattern'], exp['replace'], line)
-                    if num_subs:
-                        # use linefeed if defined in config, or default "/r/n"
-                        if(data['global_regex']['linefeed']):
-                            linefeed = data['global_regex']['linefeed']
-                        else:
-                            linefeed = '\r\n'
-                        parsed_list.append(parsed_line)
-            csv_string = linefeed.join(parsed_list)
-            logger.info(object_summary.key + " parsed successfully")
-        else:
-            csv_string = body.read()
-        # Check that the file decodes as UTF-8. If it fails move to bad and end
-        try:
-            csv_string = csv_string.decode('utf-8')
-        except UnicodeDecodeError as e:
-            e_object = e.object.splitlines()
-            logger.exception(
-                ''.join((
-                    "Decoding UTF-8 failed for file {0}\n"
-                    .format(object_summary.key),
-                    "The input file stopped parsing after line {0}:\n{1}\n"
-                    .format(len(e_object), e_object[-1]),
-                    "Keying to badfile and skipping.\n")))
-            try:
-                client.copy_object(
-                    Bucket="sp-ca-bc-gov-131565110619-12-microservices",
-                    CopySource="sp-ca-bc-gov-131565110619-12-microservices/"
-                    + object_summary.key, Key=badfile)
-            except Exception as e:
-                logger.exception("S3 transfer failed.\n{0}".format(e.message))
-            continue
-
-        # Check for an empty file. If it's empty, accept it as good and move on
-        try:
-            df = pd.read_csv(StringIO(csv_string), sep=delim, index_col=False,
-                             dtype=dtype_dic, usecols=range(column_count))
-        except Exception as e:
-            logger.exception('exption reading {0}'.format(object_summary.key))
-            if (str(e) == "No columns to parse from file"):
-                logger.warning('File is empty, keying to goodfile \
-                               and proceeding.')
-                outfile = goodfile
+    key = object_summary.key
+    # skip to next object if already processed
+    if is_processed(object_summary):
+        continue
+    else:
+        # only review those matching our configued 'doc' regex pattern
+        if re.search(doc + '$', key):
+            # under truncate, we will keep list length to 1
+            # only adding the most recently modified file to objects_to_process
+            if truncate:
+                if len(objects_to_process) == 0:
+                    objects_to_process.append(object_summary)
+                    continue
+                else:
+                    # compare last modified dates of the latest and current obj
+                    if (object_summary.last_modified
+                            > objects_to_process[0].last_modified):
+                        objects_to_process[0] = object_summary
             else:
-                logger.warning('File not empty, keying to badfile \
-                               and proceeding.')
-                outfile = badfile
-            client.copy_object(Bucket="sp-ca-bc-gov-131565110619-12-\
-                               microservices", CopySource="sp-ca-bc-gov-\
-                               131565110619-12-microservices/"
-                               + object_summary.key, Key=outfile)
-            continue
+                # no truncate, so the list may exceed 1 element
+                objects_to_process.append(object_summary)
 
-        df.columns = columns
+if truncate and len(objects_to_process) == 1:
+    logger.info(
+        'truncate is set. processing only one file: {0} (modified {1})'.format(
+            objects_to_process[0].key, objects_to_process[0].last_modified))
 
-        if 'drop_columns' in data:  # Drop any columns marked for dropping
-            df = df.drop(columns=drop_columns)
+# process the objects that were found during the earlier directory pass
+for object_summary in objects_to_process:
+    batchfile = destination + "/batch/" + object_summary.key
+    goodfile = destination + "/good/" + object_summary.key
+    badfile = destination + "/bad/" + object_summary.key
 
-        # Run replace on some fields to clean the data up
-        if 'replace' in data:
-            for thisfield in data['replace']:
-                df[thisfield['field']].replace(
-                    thisfield['old'], thisfield['new'])
+    obj = client.get_object(Bucket=bucket, Key=object_summary.key)
+    body = obj['Body']
+    # Create an object to hold the data while parsing
+    csv_string = ''
+    # Perform regex pattern replacements according to config, if defined
+    if 'global_regex' in data:
+        linefeed = ''
+        parsed_list = []
+        if(data['global_regex']['string_repl']):
+            inline_pattern = data['global_regex']['string_repl']['pattern']
+            inline_replace = data['global_regex']['string_repl']['replace']
+        body_stringified = body.read()
+        for line in body_stringified.splitlines():
+            if(data['global_regex']['string_repl']):
+                line = line.replace(inline_pattern, inline_replace)
+            for exp in data['global_regex']['regexs']:
+                parsed_line, num_subs = re.subn(
+                    exp['pattern'], exp['replace'], line)
+                if num_subs:
+                    # use linefeed if defined in config, or default "/r/n"
+                    if(data['global_regex']['linefeed']):
+                        linefeed = data['global_regex']['linefeed']
+                    else:
+                        linefeed = '\r\n'
+                    parsed_list.append(parsed_line)
+        csv_string = linefeed.join(parsed_list)
+        logger.info(object_summary.key + " parsed successfully")
+    else:
+        csv_string = body.read()
+    # Check that the file decodes as UTF-8. If it fails move to bad and end
+    try:
+        csv_string = csv_string.decode('utf-8')
+    except UnicodeDecodeError as e:
+        e_object = e.object.splitlines()
+        logger.exception(
+            ''.join((
+                "Decoding UTF-8 failed for file {0}\n"
+                .format(object_summary.key),
+                "The input file stopped parsing after line {0}:\n{1}\n"
+                .format(len(e_object), e_object[-1]),
+                "Keying to badfile and skipping.\n")))
+        try:
+            client.copy_object(
+                Bucket="sp-ca-bc-gov-131565110619-12-microservices",
+                CopySource="sp-ca-bc-gov-131565110619-12-microservices/"
+                + object_summary.key, Key=badfile)
+        except Exception as e:
+            logger.exception("S3 transfer failed.\n{0}".format(e.message))
+        continue
 
-        # Clean up date fields
-        # for each field listed in the dateformat
-        # array named "field" apply "format"
-        if 'dateformat' in data:
-            for thisfield in data['dateformat']:
-                df[thisfield['field']] = \
-                    pd.to_datetime(df[thisfield['field']],
-                                   format=thisfield['format'])
+    # Check for an empty file. If it's empty, accept it as good and move on
+    try:
+        df = pd.read_csv(StringIO(csv_string), sep=delim, index_col=False,
+                         dtype=dtype_dic, usecols=range(column_count))
+    except Exception as e:
+        logger.exception('exption reading {0}'.format(object_summary.key))
+        if (str(e) == "No columns to parse from file"):
+            logger.warning('File is empty, keying to goodfile \
+                           and proceeding.')
+            outfile = goodfile
+        else:
+            logger.warning('File not empty, keying to badfile \
+                           and proceeding.')
+            outfile = badfile
+        client.copy_object(Bucket="sp-ca-bc-gov-131565110619-12-\
+                           microservices", CopySource="sp-ca-bc-gov-\
+                           131565110619-12-microservices/"
+                           + object_summary.key, Key=outfile)
+        continue
 
-        # Put the full data set into a buffer and write it
-        # to a "|" delimited file in the batch directory
-        csv_buffer = BytesIO()
-        df.to_csv(csv_buffer, header=True, index=False, sep="|")
-        resource.Bucket(bucket).put_object(Key=batchfile,
-                                           Body=csv_buffer.getvalue())
+    df.columns = columns
 
-        # prep database call to pull the batch file into redshift
-        query = copy_query(dbtable, batchfile, log=False)
-        logquery = copy_query(dbtable, batchfile, log=True)
+    if 'drop_columns' in data:  # Drop any columns marked for dropping
+        df = df.drop(columns=drop_columns)
 
-        # if truncate is set to true, perform a transaction that will
-        # replace the existing table data with the new data in one commit
-        if (truncate):
-            scratch_start = """
+    # Run replace on some fields to clean the data up
+    if 'replace' in data:
+        for thisfield in data['replace']:
+            df[thisfield['field']].replace(
+                thisfield['old'], thisfield['new'])
+
+    # Clean up date fields
+    # for each field listed in the dateformat
+    # array named "field" apply "format"
+    if 'dateformat' in data:
+        for thisfield in data['dateformat']:
+            df[thisfield['field']] = \
+                pd.to_datetime(df[thisfield['field']],
+                               format=thisfield['format'])
+
+    # Put the full data set into a buffer and write it
+    # to a "|" delimited file in the batch directory
+    csv_buffer = BytesIO()
+    df.to_csv(csv_buffer, header=True, index=False, sep="|")
+    resource.Bucket(bucket).put_object(Key=batchfile,
+                                       Body=csv_buffer.getvalue())
+
+    # prep database call to pull the batch file into redshift
+    query = copy_query(dbtable, batchfile, log=False)
+    logquery = copy_query(dbtable, batchfile, log=True)
+
+    # if truncate is set to true, perform a transaction that will
+    # replace the existing table data with the new data in one commit
+    if (truncate):
+        scratch_start = """
 BEGIN;
 -- Clean up from last run if necessary
 DROP TABLE IF EXISTS {0}_scratch;
@@ -285,40 +290,40 @@ CREATE TABLE {0}_scratch (LIKE {0});
 ALTER TABLE {0}_scratch OWNER TO microservice;
 GRANT SELECT ON {0}_scratch TO looker;\n
 """.format(dbtable)
-            scratch_copy = copy_query(
-                dbtable + "_scratch", batchfile, log=False)
-            scratch_copy_log = copy_query(
-                dbtable + "_scratch", batchfile, log=True)
-            scratch_cleanup = """
+        scratch_copy = copy_query(
+            dbtable + "_scratch", batchfile, log=False)
+        scratch_copy_log = copy_query(
+            dbtable + "_scratch", batchfile, log=True)
+        scratch_cleanup = """
 -- Replace main table with scratch table, clean up the old table
 ALTER TABLE {0} RENAME TO {1}_old;
 ALTER TABLE {0}_scratch RENAME TO {1};
 DROP TABLE {0}_old;
 COMMIT;
 """.format(dbtable, table_name)
-            query = scratch_start + scratch_copy + scratch_cleanup
-            logquery = scratch_start + scratch_copy_log + scratch_cleanup
+        query = scratch_start + scratch_copy + scratch_cleanup
+        logquery = scratch_start + scratch_copy_log + scratch_cleanup
 
-        logger.debug(logquery)
-        with psycopg2.connect(conn_string) as conn:
-            with conn.cursor() as curs:
-                try:
-                    curs.execute(query)
-                # if the DB call fails, print error and place file in /bad
-                except psycopg2.Error as e:
-                    logger.error("Loading {0} to RedShift failed\n{1}"
-                                 .format(batchfile, e.pgerror))
-                    outfile = badfile
-                # if the DB call succeed, place file in /good
-                else:
-                    logger.info("Loaded {0} to RedShift successfully"
-                                .format(batchfile))
-                    outfile = goodfile
+    logger.debug(logquery)
+    with psycopg2.connect(conn_string) as conn:
+        with conn.cursor() as curs:
+            try:
+                curs.execute(query)
+            # if the DB call fails, print error and place file in /bad
+            except psycopg2.Error as e:
+                logger.error("Loading {0} to RedShift failed\n{1}"
+                             .format(batchfile, e.pgerror))
+                outfile = badfile
+            # if the DB call succeed, place file in /good
+            else:
+                logger.info("Loaded {0} to RedShift successfully"
+                            .format(batchfile))
+                outfile = goodfile
 
-        try:
-            client.copy_object(
-                Bucket="sp-ca-bc-gov-131565110619-12-microservices",
-                CopySource="sp-ca-bc-gov-131565110619-12-microservices/"
-                + object_summary.key, Key=outfile)
-        except boto3.exceptions.ClientError as e:
-            logger.exception("S3 transfer failed")
+    try:
+        client.copy_object(
+            Bucket="sp-ca-bc-gov-131565110619-12-microservices",
+            CopySource="sp-ca-bc-gov-131565110619-12-microservices/"
+            + object_summary.key, Key=outfile)
+    except boto3.exceptions.ClientError as e:
+        logger.exception("S3 transfer failed")

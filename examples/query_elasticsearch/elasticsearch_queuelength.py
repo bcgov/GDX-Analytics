c001c6f5a4ea7c326d2d6c64fbc60bf02edbfd81
bjenning <brendan.jennings@gov.bc.ca>
2019-07-26 13:57:39 -0700

    Added logic for querying elasticsearch and calculating queue length.

--- examples/query_elasticsearch/elasticsearch_queuelength.py
+++ examples/query_elasticsearch/elasticsearch_queuelength.py
@@ -9,6 +9,16 @@
 #                     Service BC offices to query from Elasticsearch, index and
 #                     endpoint specified.
 #
+#                     In order to query by office location (eg: Kelowna,
+#                     Kamloops), it is necessary to include a JSON export
+#                     of office names and id's. This json export can be
+#                     created in the looker explore, here:
+#                     https://analytics.gov.bc.ca/
+#                       explore/cfms_poc/cfms_poc?toggle=fil&qid=DWe1cjJI5lYB084l8fjZ1M
+#
+#                     Name the json export into a file called serviceBCOfficeList.json
+#                     and place it in the same directory as this script.
+#
 # Requirements      : Install the following python packages to allow
 #                     querying the elasticsearch endpoint, credentials, and
 #                     index for regular use:
@@ -40,6 +50,7 @@ from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Q
 import logging
 from argparse import ArgumentParser
+import json
 import sys
 import os
 import signal
@@ -93,4 +104,60 @@ index = args.index
 client = Elasticsearch(endpoint, http_auth=(http_user, http_pass))
 logger.debug('Elastic search object: ', client)
 
-for office in office:
+with open('serviceBCOfficeList.json') as json_file:
+    serviceCenters = json.load(json_file)
+
+for office in offices:
+    office_id = ''
+    for serviceCenter in serviceCenters:
+        if serviceCenter["cfms_poc.office_name"] == office:
+            office_id = serviceCenter['cfms_poc.office_id']
+
+    # Query for number of addcitizen events. Note: The derived timestamp looks
+    # weird but 'now/d+7h' is rounding down to the start of the day in UTC
+    # which is 1700 which is 17h00 local on the previous day, so I am adding
+    # 5 hours to get midnight.
+    params = Q('term', app_id='TheQ') & \
+        Q('term', event_name='addcitizen') & \
+        Q('term', **{'contexts_ca_bc_gov_cfmspoc_office_1.office_id':
+                     office_id}) & \
+        Q('range', derived_tstamp={'gte': 'now/d+7h'}) & \
+        Q('range', derived_tstamp={'lt': 'now'})
+    try:
+        s = Search(using=client, index=index).filter(params)
+    except Exception as e:
+        logger.debug(e)
+    addCitizenCount = s.count()
+
+    # Query for number of customerleft events
+    params = Q('term', app_id='TheQ') & \
+        Q('term', event_name='customerleft') & \
+        Q('term', **{'contexts_ca_bc_gov_cfmspoc_office_1.office_id':
+                     office_id}) & \
+        Q('range', derived_tstamp={'gte': 'now/d+7h'}) & \
+        Q('range', derived_tstamp={'lt': 'now'})
+    try:
+        s = Search(using=client, index=index).filter(params)
+    except Exception as e:
+        logger.debug(e)
+    customerLeft = s.count()
+
+    # Query for number of finish events
+    params = Q('term', app_id='TheQ') & \
+        Q('term', event_name='finish') & \
+        Q('term', **{'contexts_ca_bc_gov_cfmspoc_office_1.office_id':
+                     office_id}) & \
+        Q('range', derived_tstamp={'gte': 'now/d+7h'}) & \
+        Q('range', derived_tstamp={'lt': 'now'})
+    try:
+        s = Search(using=client, index=index).filter(params)
+    except Exception as e:
+        logger.debug(e)
+    finish = s.count()
+
+    # Calculate the total number of citizens in the queue for
+    # the current office
+    queueSize = addCitizenCount - (customerLeft + finish)
+
+    logger.info(office + ' queue size successfully queried')
+    print ('Office: ', office, ' Total queue size: ', queueSize)

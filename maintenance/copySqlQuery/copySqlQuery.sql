6a8d6e9d0991f695243ce01ca40576dd71b5b928
f-w <fred.wen@gov.bc.ca>
2019-10-28 15:31:56 -0700

    added auditing script

--- maintenance/copySqlQuery/copySqlQuery.sql
+++ maintenance/copySqlQuery/copySqlQuery.sql
@@ -6,6 +6,11 @@ select getdate();
 --create STL_QUERY_HISTORY if not exists
 create table IF NOT EXISTS STL_QUERY_HISTORY (like STL_QUERY including defaults);
 
+--create or replace v_stl_query_history 
+create or replace view v_stl_query_history as
+SELECT *, regexp_substr(querytxt,'"user_id":(\\d+)',1,1,'e') AS looker_user_id 
+FROM stl_query_history;
+
 --delete data older than 7 months
 delete STL_QUERY_HISTORY
   where starttime < dateadd(month, -7, CURRENT_DATE);

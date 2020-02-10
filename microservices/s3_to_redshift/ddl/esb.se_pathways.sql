2d41d01c4fa27258aea703cad93d659e2ccfcb47
bjenning <brendan.jennings@gov.bc.ca>
2020-02-10 09:57:32 -0800

    Fixed copy cmd for load_esb_se_pathways.py and added DDL for dbtable.

---
+++ microservices/s3_to_redshift/ddl/esb.se_pathways.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS esb.se_pathways
+(
+'id' VARCHAR(20) ENCODE ZSTD NOT NULL,
+'type' VARCHAR(255) ENCODE ZSTD,
+'pathway' VARCHAR(255) ENCODE ZSTD,
+'order' INT ENCODE ZSTD,
+'title' VARCHAR(255) ENCODE ZSTD
+)
+
+GRANT SELECT ON servicebc.call_centre TO "looker";
--- microservices/s3_to_redshift/load_esb_se_pathways.py
+++ microservices/s3_to_redshift/load_esb_se_pathways.py
@@ -12,72 +12,31 @@
 #               : export pgpass=<<DB_PASSWD>>
 #
 #
-# Usage         : python load_esb_se_pathways.py bucketname 
+# Usage         : python load_esb_se_pathways.py bucketname
 #
 
-import boto3  # s3 access
-from botocore.exceptions import ClientError
-import pandas as pd  # data processing
-import re  # regular expressions
-from io import StringIO
 import os  # to read environment variables
 import psycopg2  # to connect to Redshift
-import json  # to read json config files
 import sys  # to read command line parameters
-import os.path  # file handling
-import logging
 
-
-# set up logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-# create stdout handler for logs at the INFO level
-handler = logging.StreamHandler()
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter("%(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-# create file handler for logs at the DEBUG level in /logs/s3_to_redshift.log
-log_filename = '{0}'.format(os.path.basename(__file__).replace('.py', '.log'))
-handler = logging.FileHandler(os.path.join('logs', log_filename), "a",
-                              encoding=None, delay="true")
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(levelname)s:%(name)s:%(asctime)s:%(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
 # Take one argument: bucketname
 if (len(sys.argv) != 2):
     print "Usage: python load_esb_se_pathways.py bucketname"
     sys.exit(1)
-bucket = sys.argv[1]
+bucket_name = sys.argv[1]
 
-source = 'brendan_test'
 filename = 'se_pathways.csv'
+dbtable = 'esb.se_pathways'
+aws_key = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
 
-# set up S3 connection
-client = boto3.client('s3')  # low-level functional API
-resource = boto3.resource('s3')  # high-level object-oriented API
-my_bucket = resource.Bucket(bucket)  # subsitute this for your s3 bucket name.
-key = ''
+# Constructs the database copy query string
+query = """ COPY {0}\nFROM 's3://{1}/{2}'\n\
+    CREDENTIALS 'aws_access_key_id={3};aws_secret_access_key={4}' CSV;\n
+    """.format(dbtable, bucket_name, filename, aws_key, aws_secret_key)
 
-# find the latest file matching filename se_pathways.csv
-for object_summary in sorted(my_bucket.objects.filter(Prefix=source),key=attrgetter('last_modified'),reverse=True):
-    if re.search(filename, object_summary.key):
-        logger.info('\nhttps://s3.console.aws.amazon.com/s3/object/sp-ca-bc-gov-131565110619-12-microservices/'
-                    + object_summary.key
-                    + '\n' + object_summary.last_modified
-                    + '\n---')
-        key = object_summary.key
-        break
-
-obj = client.get_object(Bucket=bucket, Key=object_summary.key)
-body = obj['Body']
-query = body.read().decode('utf-8')
-
-# prep database call to pull the batch file into redshift
+# Database connection string
 conn_string = """
 dbname='{dbname}' host='{host}' port='{port}' user='{user}' password={password}
 """.format(dbname='snowplow',
@@ -86,12 +45,16 @@ dbname='{dbname}' host='{host}' port='{port}' user='{user}' password={password}
            user=os.environ['pguser'],
            password=os.environ['pgpass'])
 
+# Execute the transaction against Redshift using the psycopg2 library
 with psycopg2.connect(conn_string) as conn:
     with conn.cursor() as curs:
         try:
             curs.execute(query)
-        except psycopg2.Error as e: # if the DB call fails, print error and place file in /bad
-            logger.exception("Loading failed\n{0}".format(e.pgerror))
+        # if the DB call fails, print error msg
+        except psycopg2.Error as e:
+            print ("Loading {0} to RedShift failed\n{1}"
+                   .format(filename, e.pgerror))
+        # if the DB call succeeds, print success msg
         else:
-            logger.info("Loaded successfully")
-
+            print ("Loaded {0} to RedShift successfully"
+                   .format(filename))

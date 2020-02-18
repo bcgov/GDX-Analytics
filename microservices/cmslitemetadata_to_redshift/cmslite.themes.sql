625f9e834f11f01350bae3e0d0f138e2706ba827
Dan Pollock <dan.pollock@gov.bc.ca>
2020-02-18 20:44:00 +0000

    add cmslite.themes.sql ddl

---
+++ microservices/cmslitemetadata_to_redshift/cmslite.themes.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS cmslite.themes (
+	"node_id"	VARCHAR(255),
+	"title"		VARCHAR(2047),
+	"hr_url"	VARCHAR(2047),
+	"parent_node_id" VARCHAR(255),
+	"theme_id"	VARCHAR(255),
+	"subtheme_id"	VARCHAR(255),
+	"topic_id"	VARCHAR(255),
+	"theme"		VARCHAR(2047),
+	"subtheme"	VARCHAR(2047),
+	"topic"		VARCHAR(2047)
+);      
+ALTER TABLE cmslite.themes OWNER TO microservice;
+GRANT SELECT ON cmslite.themes TO looker;

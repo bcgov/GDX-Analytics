b8c55b645d1e2bfde444539d2d0d61935738af98
Mark Walle <Mark.Walle@gov.bc.ca>
2020-02-26 09:55:19 -0800

    Preparing ddls and configs for Service BC SFTS
    These DDLs have been run in RedShift as the microservice user.

---
+++ microservices/s3_to_redshift/ddl/servicebc.call_centre_csat.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS servicebc.call_centre_csat (
+  "ID" VARCHAR(20) ENCODE ZSTD NOT NULL PRIMARY KEY,
+  "DateKey" INTEGER ENCODE ZSTD,
+  "TimeKey" INTEGER ENCODE ZSTD,
+  "SkillGroupChannel" VARCHAR(30) ENCODE ZSTD,
+  "AltSkillGroupChannel" VARCHAR(30) ENCODE ZSTD,
+  "ANI" VARCHAR(50) ENCODE ZSTD,
+  "PeripheralCallType" INTEGER ENCODE ZSTD,
+  "Transfered" BOOLEAN ENCODE ZSTD,
+  "DigitsDialed" VARCHAR(30) ENCODE ZSTD,
+  "SurveyChannel" VARCHAR(30) ENCODE ZSTD,
+  "S1CSAT" FLOAT ENCODE ZSTD,
+  "S2CSAT" FLOAT ENCODE ZSTD,
+  "S3CSAT" FLOAT ENCODE ZSTD,
+  "S4CSAT" FLOAT ENCODE ZSTD,
+  "S5CSAT" FLOAT ENCODE ZSTD
+);
+
+
+GRANT SELECT ON servicebc.call_centre_csat TO "looker";
+GRANT SELECT ON servicebc.call_centre_csat TO "datamodeling";
---
+++ microservices/s3_to_redshift/ddl/servicebc.call_centre_esb_data.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS servicebc.call_centre_esb_data (
+  "ID" DECIMAL(13,0) NOT NULL PRIMARY KEY ENCODE ZSTD,
+  "SkillGroup" VARCHAR(50) ENCODE ZSTD,
+  "ContactReason" VARCHAR(50) ENCODE ZSTD,
+  "ContactReasonDetail" VARCHAR(50) ENCODE ZSTD,
+  "DateKey" INTEGER ENCODE ZSTD,
+  "INTEGERerval" VARCHAR(5) ENCODE ZSTD,
+  "AnsweredWithinServiceLevel" BOOLEAN ENCODE ZSTD,
+  "CallPutOnHold" INTEGER ENCODE ZSTD,
+  "HandleTime" FLOAT ENCODE ZSTD,
+  "WaitTime" FLOAT ENCODE ZSTD,
+  "WrapTime" FLOAT ENCODE ZSTD,
+  "TalkTime" FLOAT ENCODE ZSTD,
+  "HoldTime" FLOAT ENCODE ZSTD,
+  "IncomingPhoneNumber" VARCHAR(20) ENCODE ZSTD
+);
+
+GRANT SELECT ON servicebc.call_centre_esb_data TO "looker";
+GRANT SELECT ON servicebc.call_centre_esb_data TO "datamodeling";

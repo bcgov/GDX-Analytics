8c98a42f8caa55c48300c1798582c9415a86cfa4
Mark Walle <Mark.Walle@gov.bc.ca>
2020-02-26 09:55:19 -0800

    Preparing ddls and configs for Service BC SFTS
    These DDLs have been run in RedShift as the microservice user.

---
+++ microservices/s3_to_redshift/ddl/servicebc.call_centre_calls.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS servicebc.call_centre_calls (
+  "ID" INTEGER ENCODE ZSTD NOT NULL PRIMARY KEY,
+  "Datekey" INTEGER ENCODE ZSTD,
+  "TimeKey" INTEGER ENCODE ZSTD,
+  "UWFAgentID" INTEGER ENCODE ZSTD,
+  "Instance" VARCHAR(20) ENCODE ZSTD,
+  "ContactReason" VARCHAR(50) ENCODE ZSTD,
+  "ContactReasonDetail" VARCHAR(50) ENCODE ZSTD,
+  "WrapTime" FLOAT ENCODE ZSTD,
+  "ActiveTime" FLOAT ENCODE ZSTD,
+  "PresentingTime" FLOAT ENCODE ZSTD,
+  "SuspendTime" FLOAT ENCODE ZSTD,
+  "SuspendCount" INTEGER ENCODE ZSTD,
+  "TSFFlag" INTEGER ENCODE ZSTD
+);
+
+GRANT SELECT ON servicebc.call_centre_calls TO "looker";
+GRANT SELECT ON servicebc.call_centre_calls TO "datamodeling";

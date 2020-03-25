561e445294cb79c519386542496f709c26f65bd1
bjenning <brendan.jennings@gov.bc.ca>
2020-03-25 16:13:35 -0700

    Removed typo, corrected timestamp format

--- microservices/s3_to_redshift/ddl/workbc.asset_downloads.sql
+++ microservices/s3_to_redshift/ddl/workbc.asset_downloads.sql
@@ -1,13 +1,12 @@
 CREATE TABLE IF NOT EXISTS workbc.asset_downloads
 ( 
   "ip_address" VARCHAR(255) ENCODE ZSTD NOT NULL,
-  "date_timestamp" timestamp without time zone ENCODE ZSTD,
+  "date_timestamp" VARCHAR(255)  ENCODE ZSTD,
   "request_string" VARCHAR(4095) ENCODE ZSTD,
   "status_code" DOUBLE PRECISION ENCODE ZSTD,
   "return_size" DOUBLE PRECISION ENCODE ZSTD,
   "referrer" VARCHAR(4095) ENCODE ZSTD,
   "user_agent_http_request_header" VARCHAR(4095) ENCODE ZSTD,
-  "proxy" VARCHAR(255) ENCODE ZSTD,
   "os_family" VARCHAR(255) ENCODE ZSTD,
   "os_version" VARCHAR(255) ENCODE ZSTD,
   "browser_family" VARCHAR(255) ENCODE ZSTD,

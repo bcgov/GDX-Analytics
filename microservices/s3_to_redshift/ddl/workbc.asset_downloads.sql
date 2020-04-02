a1438438ccb0831a9598f77a180fbd1a03fb124e
bjenning <brendan.jennings@gov.bc.ca>
2020-04-02 14:59:05 -0700

    Changed column order in ddl for workbc.asset_downloads.

--- microservices/s3_to_redshift/ddl/workbc.asset_downloads.sql
+++ microservices/s3_to_redshift/ddl/workbc.asset_downloads.sql
@@ -7,13 +7,13 @@ CREATE TABLE IF NOT EXISTS workbc.asset_downloads
   "return_size" DOUBLE PRECISION ENCODE ZSTD,
   "referrer" VARCHAR(4095) ENCODE ZSTD,
   "user_agent_http_request_header" VARCHAR(4095) ENCODE ZSTD,
+  "proxy" VARCHAR(255)  ENCODE ZSTD,
   "os_family" VARCHAR(255) ENCODE ZSTD,
   "os_version" VARCHAR(255) ENCODE ZSTD,
   "browser_family" VARCHAR(255) ENCODE ZSTD,
   "browser_version" VARCHAR(255) ENCODE ZSTD,
   "referrer_source" VARCHAR(255) ENCODE ZSTD,
-  "referrer_medium" VARCHAR(255) ENCODE ZSTD,
-  "proxy" VARCHAR(255) ENCODE ZSTD
+  "referrer_medium" VARCHAR(255) ENCODE ZSTD
 );
 
 GRANT SELECT ON workbc.asset_downloads TO "looker";

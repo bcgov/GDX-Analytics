3307dc48292b674ddef2ddab937f87ff96c161dd
stephanieyurchak <Stephanie.Yurchak@gov.bc.ca>
2020-07-03 10:16:24 -0700

    Add compressions to cmslite.metadata.sql

--- microservices/cmslitemetadata_to_redshift/ddl/cmslite.metadata.sql
+++ microservices/cmslitemetadata_to_redshift/ddl/cmslite.metadata.sql
@@ -2,26 +2,26 @@ CREATE SCHEMA IF NOT EXISTS cmslite;
 
 -- LOOKUP TABLE FOR VALUES
 
--- SY: dbtables_dictionaries - Could move to their own ddl file (e.g., cmslite.dictionaries.sql)?
+-- SY: dbtables_dictionaries
 
 CREATE TABLE IF NOT EXISTS cmslite.content_types (
-    "id"      BIGINT          NOT NULL,
-    "value"   VARCHAR(255)     NOT NULL
+    "id"      BIGINT		ENCODE LZO	NOT NULL,
+    "value"   VARCHAR(255)  ENCODE ZSTD	NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cmslite.mbcterms_subject_categories (
-    "id"      BIGINT          NOT NULL,
-    "value"   VARCHAR(255)     NOT NULL
+    "id"      BIGINT		ENCODE LZO	NOT NULL,
+    "value"   VARCHAR(255)  ENCODE ZSTD	NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cmslite.dcterms_subjects (
-    "id"      BIGINT          NOT NULL,
-    "value"   VARCHAR(255)     NOT NULL
+    "id"      BIGINT		ENCODE LZO	NOT NULL,
+    "value"   VARCHAR(255)  ENCODE ZSTD	NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cmslite.dcterms_languages (
-    "id"      BIGINT          NOT NULL,
-    "value"   VARCHAR(255)     NOT NULL
+    "id"      BIGINT		ENCODE LZO	NOT NULL,
+    "value"   VARCHAR(255)  ENCODE ZSTD	NOT NULL
 );
 
 -- SY: Table no longer in cmslite schema
@@ -37,38 +37,38 @@ CREATE TABLE IF NOT EXISTS cmslite.dcterms_languages (
 ); */
 
 CREATE TABLE IF NOT EXISTS cmslite.audiences (
-    "id"      BIGINT          NOT NULL,
-    "value"   VARCHAR(255)     NOT NULL
+    "id"      BIGINT		ENCODE LZO	NOT NULL,
+    "value"   VARCHAR(255)  ENCODE ZSTD	NOT NULL
 );
 
 -- SY: Added this table to reflect cmslite_gdx.json
 CREATE TABLE IF NOT EXISTS cmslite.creators (
-    "id"      BIGINT          NOT NULL,
-    "value"   VARCHAR(255)     NOT NULL
+    "id"      BIGINT		ENCODE LZO	NOT NULL,
+    "value"   VARCHAR(255)  ENCODE ZSTD	NOT NULL
 );
 
 -- LOOKUP TABLES (MANY-TO-MANY)
 
--- SY: dbtables_metadata - Keep in cmslite.metadata.sql
+-- SY: dbtables_metadata
 
 CREATE TABLE IF NOT EXISTS cmslite.metadata_content_types (
-    "node_id"   VARCHAR(255)    NOT NULL,
-    "id"        BIGINT          NOT NULL
+    "node_id"   VARCHAR(255)    ENCODE LZO		NOT NULL,
+    "id"        BIGINT          ENCODE DELTA	NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cmslite.metadata_subject_categories (
-    "node_id"   VARCHAR(255)    NOT NULL,
-    "id"        BIGINT          NOT NULL
+    "node_id"   VARCHAR(255)    ENCODE LZO		NOT NULL,
+    "id"        BIGINT          ENCODE DELTA	NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cmslite.metadata_subjects (
-    "node_id"   VARCHAR(255)    NOT NULL,
-    "id"        BIGINT          NOT NULL
+    "node_id"   VARCHAR(255)    ENCODE LZO		NOT NULL,
+    "id"        BIGINT          ENCODE DELTA	NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cmslite.metadata_languages (
-    "node_id"   VARCHAR(255)    NOT NULL,
-    "id"        BIGINT          NOT NULL
+     "node_id"   VARCHAR(255)    ENCODE LZO		NOT NULL,
+    "id"        BIGINT          ENCODE DELTA	NOT NULL
 );
 
 -- SY: Table no longer in cmslite schema
@@ -84,14 +84,14 @@ CREATE TABLE IF NOT EXISTS cmslite.metadata_languages (
 ); */
 
 CREATE TABLE IF NOT EXISTS cmslite.metadata_audiences (
-    "node_id"   VARCHAR(255)    NOT NULL,
-    "id"        BIGINT          NOT NULL
+    "node_id"   VARCHAR(255)    ENCODE LZO		NOT NULL,
+    "id"        BIGINT          ENCODE DELTA	NOT NULL
 );
 
 -- SY: Added this table to reflect cmslite_gdx.json
 CREATE TABLE IF NOT EXISTS cmslite.metadata_creators (
-    "node_id"   VARCHAR(255)    NOT NULL,
-    "id"        BIGINT          NOT NULL
+    "node_id"   VARCHAR(255)    ENCODE LZO		NOT NULL,
+    "id"        BIGINT          ENCODE DELTA	NOT NULL
 );
 
 -- METADATA TABLE FOR NODE_ID RECORDS
@@ -116,39 +116,39 @@ CREATE TABLE IF NOT EXISTS cmslite.metadata_creators (
 -- SY: Not sure if node_id needs NOT NULL but kept it as it was in the old table above
 -- SY: sitekey in Looker but site_key in cmslite_gdx.json??
 CREATE TABLE IF NOT EXISTS cmslite.metadata (
-	"node_id"						VARCHAR(255)	NOT NULL,
-	"parent_node_id"				VARCHAR(255),
-	"ancestor_nodes"				VARCHAR(4095),	
-	"hr_url"						VARCHAR(2047),	
-	"keywords"						VARCHAR(1023),
-	"description"					VARCHAR(1023),
-	"page_type"						VARCHAR(255),
-	"synonyms"						VARCHAR(1023),
-	"dcterms_creator"				VARCHAR(4095),
-	"modified_date"					TIMESTAMP,	
-	"created_date"					TIMESTAMP,
-	"updated_date"					TIMESTAMP,
-	"published_date"				TIMESTAMP,
-	"title"							VARCHAR(2047),
-	"nav_title"						VARCHAR(2047),
-	"eng_nav_title"					VARCHAR(2047),
-	"sitekey"						VARCHAR(255),
-	"site_id"						VARCHAR(255),
-	"language_name"					VARCHAR(256),
-	"language_code"					VARCHAR(256),
-	"page_status" 					VARCHAR(255),
-	"published_by" 					VARCHAR(255),
-	"created_by" 					VARCHAR(255),
-	"modified_by" 					VARCHAR(255),
-	"node_level" 					VARCHAR(10),
-	"locked_date"					TIMESTAMP,
-	"moved_date"					TIMESTAMP,
-	"exclude_from_ia" 				VARCHAR(10),
-	"hide_from_navigation"			VARCHAR(10),
-	"exclude_from_search_engines"	VARCHAR(10),
-	"security_classification"		VARCHAR(255),
-	"security_label"				VARCHAR(255),
-	"publication_date"				TIMESTAMP,
-	"defined_security_groups"		VARCHAR(1023),
-	"inherited_security_group"		VARCHAR(1023),
+"node_id"						VARCHAR(255)					ENCODE	LZO		NOT NULL,
+"parent_node_id"				VARCHAR(255)					ENCODE	ZSTD,	
+"ancestor_nodes"				VARCHAR(4095)					ENCODE	ZSTD,	
+"hr_url"						VARCHAR(2047)					ENCODE	ZSTD,	
+"keywords"						VARCHAR(1023)					ENCODE	ZSTD,	
+"description"					VARCHAR(1023)					ENCODE	ZSTD,	
+"page_type"						VARCHAR(255)					ENCODE	ZSTD,	
+"synonyms"						VARCHAR(1023)					ENCODE	ZSTD,	
+"dcterms_creator"				VARCHAR(4095)					ENCODE	ZSTD,	
+"modified_date"					TIMESTAMP WITHOUT TIME ZONE		ENCODE	LZO,	
+"created_date"					TIMESTAMP WITHOUT TIME ZONE		ENCODE	LZO,	
+"updated_date"					TIMESTAMP WITHOUT TIME ZONE		ENCODE	LZO,	
+"published_date"				TIMESTAMP WITHOUT TIME ZONE		ENCODE	LZO,	
+"title"							VARCHAR(2047)					ENCODE	ZSTD,	
+"nav_title"						VARCHAR(2047)					ENCODE	ZSTD,	
+"eng_nav_title"					VARCHAR(2047)					ENCODE	ZSTD,	
+"sitekey"						VARCHAR(255)					ENCODE	LZO,	
+"site_id"						VARCHAR(255)					ENCODE	LZO,	
+"language_name"					VARCHAR(256)					ENCODE	LZO,	
+"language_code"					VARCHAR(256)					ENCODE	LZO,	
+"page_status"					VARCHAR(255)					ENCODE	LZO,	
+"published_by"					VARCHAR(255)					ENCODE	LZO,	
+"created_by"					VARCHAR(255)					ENCODE	LZO,	
+"modified_by"					VARCHAR(255)					ENCODE	LZO,	
+"node_level"					VARCHAR(10)						ENCODE	LZO,	
+"locked_date"					TIMESTAMP WITHOUT TIME ZONE		ENCODE	AZ64,	
+"moved_date"					TIMESTAMP WITHOUT TIME ZONE		ENCODE	AZ64,	
+"exclude_from_ia"				VARCHAR(10)						ENCODE	LZO,	
+"hide_from_navigation"			VARCHAR(10)						ENCODE	LZO,	
+"exclude_from_search_engines"	VARCHAR(10)						ENCODE	LZO,	
+"security_classification"		VARCHAR(255)					ENCODE	LZO,	
+"security_label"				VARCHAR(255)					ENCODE	LZO,	
+"publication_date"				TIMESTAMP WITHOUT TIME ZONE		ENCODE	AZ64,	
+"defined_security_groups"		VARCHAR(1023)					ENCODE	LZO,	
+"inherited_security_group"		VARCHAR(1023)					ENCODE	LZO
 };

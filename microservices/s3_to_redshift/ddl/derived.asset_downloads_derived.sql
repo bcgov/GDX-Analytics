17ff5898ab0ca2420a093eab306f2bb4e07f5fa3
bjenning <brendan.jennings@gov.bc.ca>
2020-07-24 15:12:05 -0700

    Added ddl's for combined assets tables.

---
+++ microservices/s3_to_redshift/ddl/derived.asset_downloads_derived.sql
@@ -0,0 +1,38 @@
+CREATE TABLE IF NOT EXISTS derived.asset_downloads_derived
+( 
+  "asset_url" VARCHAR(4095) ENCODE ZSTD,
+  "date_timestamp" timestamp without time zone ENCODE ZSTD,
+  "ip_address" VARCHAR(255) ENCODE ZSTD NOT NULL,
+  "request_response_time" VARCHAR(255) ENCODE ZSTD,
+  "referrer" VARCHAR(4095) ENCODE ZSTD,
+  "return_size" DOUBLE PRECISION ENCODE ZSTD,
+  "status_code" VARCHAR(255) ENCODE ZSTD,
+  "asset_file" VARCHAR(4095) ENCODE ZSTD,
+  "asset_ext" VARCHAR(255) ENCODE ZSTD,
+  "user_agent_http_request_header" VARCHAR(4095) ENCODE ZSTD,
+  "request_string" VARCHAR(4095) ENCODE ZSTD,
+  "asset_host" VARCHAR(255) ENCODE ZSTD,
+  "asset_source" VARCHAR(255) ENCODE ZSTD,
+  "direct_download" BOOL ENCODE ZSTD,
+  "offsite_download" BOOL ENCODE ZSTD,
+  "is_efficiencybc_dev" BOOL ENCODE ZSTD,
+  "is_government" BOOL ENCODE ZSTD,
+  "is_mobile" BOOL ENCODE ZSTD,
+  "device" VARCHAR(255) ENCODE ZSTD,
+  "os_family" VARCHAR(255) ENCODE ZSTD,
+  "os_version" VARCHAR(255) ENCODE ZSTD,
+  "browser_family" VARCHAR(255) ENCODE ZSTD,
+  "browser_version" VARCHAR(255) ENCODE ZSTD,
+  "referrer_urlhost_derived" VARCHAR(255) ENCODE ZSTD,
+  "referrer_medium" VARCHAR(255) ENCODE ZSTD,
+  "referrer_urlpath" VARCHAR(4095) ENCODE ZSTD,
+  "referrer_urlquery" VARCHAR(4095) ENCODE ZSTD,
+  "referrer_urlscheme" VARCHAR(4095) ENCODE ZSTD,
+  "page_referrer_display_url" VARCHAR(4095) ENCODE ZSTD,
+  "asset_url_case_insensitive" VARCHAR(4095) ENCODE ZSTD,
+  "asset_url_nopar" VARCHAR(4095) ENCODE ZSTD,
+  "asset_url_nopar_case_insensitive" VARCHAR(4095) ENCODE ZSTD,
+  "truncated_asset_url_nopar_case_insensitive" VARCHAR(4095) ENCODE ZSTD
+);
+
+GRANT SELECT ON derived.asset_downloads_derived TO "looker";

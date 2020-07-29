06aa7bd7246fb6d46eeb9e91fe333a41ab92cbf1
ryrjanes <36681256+ryrjanes@users.noreply.github.com>
2020-07-29 15:35:11 -0700

    Update wordpress_sp_script.js

--- web_trackers/wordpress_sp_script.js
+++ web_trackers/wordpress_sp_script.js
@@ -1,4 +1,4 @@
-// <!-- Snowplow starts plowing - Wordpress vA.2.10.2 -->
+// <!-- Snowplow starts plowing - Wordpress vE.2.14.0 -->
 ;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
     p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)
     };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;

9b591a70e92a2ca2cd2ea87226ab3718d1aa9adf
bjenning <brendan.jennings@gov.bc.ca>
2020-08-24 11:37:48 -0700

    Updated ddl to match derived data.

--- microservices/s3_to_redshift/ddl/derived.asset_downloads_derived.sql
+++ microservices/s3_to_redshift/ddl/derived.asset_downloads_derived.sql
@@ -1,18 +1,18 @@
-CREATE TABLE IF NOT EXISTS derived.asset_downloads_derived
+CREATE TABLE IF NOT EXISTS test.asset_downloads_derived
 ( 
-  "asset_url" VARCHAR(4095) ENCODE ZSTD,
+  "asset_url" VARCHAR(4200) ENCODE ZSTD,
   "date_timestamp" timestamp without time zone ENCODE ZSTD,
   "ip_address" VARCHAR(255) ENCODE ZSTD NOT NULL,
   "request_response_time" VARCHAR(255) ENCODE ZSTD,
   "referrer" VARCHAR(4095) ENCODE ZSTD,
   "return_size" DOUBLE PRECISION ENCODE ZSTD,
   "status_code" INTEGER ENCODE ZSTD,
-  "asset_file" VARCHAR(4095) ENCODE ZSTD,
+  "asset_file" VARCHAR(4200) ENCODE ZSTD,
   "asset_ext" VARCHAR(4200) ENCODE ZSTD,
   "user_agent_http_request_header" VARCHAR(4095) ENCODE ZSTD,
   "request_string" VARCHAR(4095) ENCODE ZSTD,
-  "asset_host" VARCHAR(255) ENCODE ZSTD,
-  "asset_source" VARCHAR(255) ENCODE ZSTD,
+  "asset_host" VARCHAR(4117) ENCODE ZSTD NOT NULL,
+  "asset_source" VARCHAR(4117) ENCODE ZSTD NOT NULL,
   "direct_download" BOOL ENCODE ZSTD,
   "offsite_download" BOOL ENCODE ZSTD,
   "is_efficiencybc_dev" BOOL ENCODE ZSTD,
@@ -35,4 +35,4 @@ CREATE TABLE IF NOT EXISTS derived.asset_downloads_derived
   "truncated_asset_url_nopar_case_insensitive" VARCHAR(4095) ENCODE ZSTD
 );
 
-GRANT SELECT ON derived.asset_downloads_derived TO "looker";
+GRANT SELECT ON test.asset_downloads_derived TO "looker";

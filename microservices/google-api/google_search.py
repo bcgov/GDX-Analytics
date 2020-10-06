e9d862bd36ab5c0cea31e5c9692ab7659324d729
Mark Walle <Mark.Walle@gov.bc.ca>
2020-10-06 12:00:31 -0700

    correct the handler for signal to the function name

--- microservices/google-api/google_search.py
+++ microservices/google-api/google_search.py
@@ -80,7 +80,7 @@ import lib.logs as log
 def signal_handler(sig, frame):
     logger.debug('Ctrl+C pressed!')
     sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler())
+signal.signal(signal.SIGINT, signal_handler)
 
 
 logger = logging.getLogger(__name__)

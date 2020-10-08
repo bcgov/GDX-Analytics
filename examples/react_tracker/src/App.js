19455aff0ee31a9848084313cb32c975ae8bade5
bjenning <brendan.jennings@gov.bc.ca>
2020-10-08 11:35:37 -0700

    Initial install of Snowplow Standalone web tracker vE.2.14.0.

--- examples/react_tracker/src/App.js
+++ examples/react_tracker/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Home</h1>
+        <p>
+          This is an example of a single page web app in react. It has the Snowplow Javascript Standalone web tracker vE.2.14.0 Installed.
+        </p>
       </div>
     )
   }

251410297427f2a2adcb1e7bfc443b071b036fcd
Mark Walle <Mark.Walle@gov.bc.ca>
2020-11-02 15:52:37 -0800

    fix group_by after removing hidden column

--- microservices/sfts/dml/pmrp_qdata_range.sql
+++ microservices/sfts/dml/pmrp_qdata_range.sql
@@ -41,6 +41,6 @@ SELECT * FROM(
     FROM derived.theq_step1  AS cfms_poc
     WHERE cfms_poc.welcome_time BETWEEN {pmrp_date_range}
     AND (((TRANSLATE(TRANSLATE(cfms_poc.office_name, '' '', ''_''),''.'','''')) LIKE ''%''))
-    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28
     ORDER BY 14
 )

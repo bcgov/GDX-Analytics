ec98b585f1a8320c564b186d5243fec14e80f8cb
bjenning <brendan.jennings@gov.bc.ca>
2020-11-18 11:23:33 -0800

    Added ddl's for user and group data.

---
+++ microservices/s3_to_redshift/ddl/cmslite.user_activity.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS cmslite.user_activity
+( 
+  "user_id" VARCHAR(100) ENCODE ZSTD NOT NULL,
+  "user_idir" VARCHAR(100) ENCODE ZSTD,
+  "activity_type" VARCHAR(255) ENCODE ZSTD,
+  "memo" VARCHAR(255) ENCODE ZSTD,
+  "activity_date" TIMESTAMP ENCODE ZSTD
+);
+
+GRANT SELECT ON cmslite.user_activity TO "looker";

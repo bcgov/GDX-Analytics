9d17aba659f4eb4a78cefe7860304f5f532736da
Veenu Veenu <veenu.veenu@gov.bc.ca>
2021-09-22 12:55:52 -0700

    added version as an argument for filename

--- maintenance/backup_looker_dev_database.sh
+++ maintenance/backup_looker_dev_database.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
-mysqldump looker_dev > ./tmp/backup-$(date +%Y-%m-%d).sql --set-gtid-purged=OFF
-aws s3 cp ./tmp/backup-$(date +%Y-%m-%d).sql s3://ca-bc-gov-analytics-lookerdb-backup/looker-dev-database-backup/
-rm ./tmp/backup-$(date +%Y-%m-%d).sql
+################
+#
+#Filename consists of version number that was passed as an argument and then appending today's date to keep track of files.
+#This script performs following steps in order.Use it to backup looker dev database.
+#Step1 -Backup Looker's dev database from RDS to local tmp directory.
+#Step2 -Upload that backup to store in AWS s3 bucket.
+#Step3 -Remove it from tmp directory for cleaning purposes
+#
+#
+#
+#Usage :backup_looker_dev_database.sh arg1
+#Where arg1 = Current looker dev version
+#For example if looker dev version is 21.20.10 then command would be backup_looker_dev_database.sh 21.20.10
+#################
+
+
+version=$1
+
+mysqldump looker_dev > ./tmp/looker_dev_backup_v${version}_$(date +%Y-%m-%d).sql --set-gtid-purged=OFF
+aws s3 cp ./tmp/looker_dev_backup_v${version}_$(date +%Y-%m-%d).sql s3://ca-bc-gov-analytics-lookerdb-backup/looker-dev-database-backup/
+rm ./tmp/looker_dev_backup_v${version}_$(date +%Y-%m-%d).sql

ad5c43b799f76899bc541ab250a313a71c37e677
Mark Walle <mark.walle@gov.bc.ca>
2021-09-22 14:27:42 -0700

    updating documentation in backup_looker_dev_database script for readability

--- maintenance/backup_looker_dev_database.sh
+++ maintenance/backup_looker_dev_database.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 ################
 #
-#Filename consists of version number that was passed as an argument and then appending today's date to keep track of files.
-#This script performs following steps in order.Use it to backup looker dev database.
-#Step1 -Backup Looker's dev database from RDS to local tmp directory.
-#Step2 -Upload that backup to store in AWS s3 bucket.
-#Step3 -Remove it from tmp directory for cleaning purposes
+# Filename consists of version number that was passed as an argument and then appending today's date to keep track of files.
+# This script performs following steps in order.Use it to backup looker dev database.
+# Step1 - Backup Looker's dev database from RDS to local tmp directory.
+# Step2 - Upload that backup to store in AWS s3 bucket.
+# Step3 - Remove it from tmp directory for cleaning purposes
 #
 #
 #
-#Usage :backup_looker_dev_database.sh arg1
-#Where arg1 = Current looker dev version
-#For example if looker dev version is 21.20.10 then command would be backup_looker_dev_database.sh 21.20.10
+# Usage : backup_looker_dev_database.sh arg1
+#  Where arg1 is the version number of the current Looker Dev.
+#  For example: if the Looker Dev version being backed up is 21.20.10 then,
+#               pass that as arg1, as in:
+#               backup_looker_dev_database.sh 21.20.10
 #################
 
 

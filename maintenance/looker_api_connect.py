2d740cc3dcc30ce2182a4b1ee4fbaeb375faeb28
Veenu Punyani <Veenu.veenu@gov.bc.ca>
2022-01-20 14:27:02 -0800

    Update looker_api_connect.py

--- maintenance/looker_api_connect.py
+++ maintenance/looker_api_connect.py
@@ -3,10 +3,10 @@ import os
 import json
 import time
 sdk = looker_sdk.init40("/Users/vveenu/Applications/looker_api/looker.ini")
-toc = time.perf_counter()
+before = time.perf_counter()
 response = sdk.run_query(
     query_id=742831,
     result_format="json_detail",)
 
-tic = time.perf_counter()
-print(f"Downloaded the tutorial in {tic - toc:0.4f} seconds")
+after = time.perf_counter()
+print(f"Query was run in {after - before:0.4f} seconds")

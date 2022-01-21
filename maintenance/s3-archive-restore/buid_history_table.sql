00587ffb3667c0cb1fa079f556d015f43aded496
Lane Clark <lane.clark@gov.bc.ca>
2022-01-21 09:47:55 -0800

    Add build_history_table.sql

---
+++ maintenance/s3-archive-restore/buid_history_table.sql
@@ -0,0 +1,25 @@
+-- Build a CTE from atmoic data and SELECT columns required to build PDTs
+-- Where the min root_tstamp is the oldest date in the corresponding business areas atomic table.
+-- Where the max root_tstamp is up to but not including the first date where data is not yet archived
+WITH custom_events AS
+(
+    SELECT root_id, root_tstamp, 'atomic.ca_bc_gov_workbc_find_career_1' AS table_name FROM atomic.ca_bc_gov_workbc_find_career_1 WHERE root_tstamp < '2021-10-01' AND root_tstamp >= '2021-09-01'
+UNION
+    SELECT root_id, root_tstamp, 'atomic.ca_bc_gov_cfmspoc_appointment_step_1' AS table_name FROM atomic.ca_bc_gov_cfmspoc_appointment_step_1 WHERE root_tstamp < '2021-10-01' AND root_tstamp >= '2021-09-01'
+UNION
+    SELECT root_id, root_tstamp, 'atomic.ca_bc_gov_ldb_click_1' AS table_name FROM atomic.ca_bc_gov_ldb_click_1 WHERE root_tstamp < '2021-10-01' AND root_tstamp >= '2021-09-01'    
+    
+)
+
+SELECT 
+  wp.id AS page_view_id,
+  wp.root_tstamp,
+  wp.root_id,
+  COALESCE(events.page_urlhost,'') AS page_urlhost,
+  events.page_url,
+  events.domain_sessionid,
+  table_name -- debug column for understandability
+FROM custom_events AS ce
+          JOIN atomic.com_snowplowanalytics_snowplow_web_page_1 AS wp
+              ON ce.root_id = wp.root_id AND ce.root_tstamp = wp.root_tstamp
+          LEFT JOIN atomic.events ON ce.root_id = events.event_id AND ce.root_tstamp = events.collector_tstamp)

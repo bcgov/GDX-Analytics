d16fbcd31d21054e61470ef9fc37faf9680e7c27
Veenu Punyani <Veenu.veenu@gov.bc.ca>
2022-01-21 15:12:55 -0800

    updated readme and also added arg1 as query id

--- maintenance/looker_api/looker_api_connect.py
+++ maintenance/looker_api/looker_api_connect.py
@@ -2,6 +2,9 @@ import looker_sdk
 import os
 import json
 import time
+import sys
+query = int(sys.argv[1])
+
 sdk = looker_sdk.init40("looker.ini")
 before = time.perf_counter()
 response = sdk.run_query(
@@ -10,3 +13,4 @@ response = sdk.run_query(
 
 after = time.perf_counter()
 print(f"Query was run in {after - before:0.4f} seconds")
+print(query)

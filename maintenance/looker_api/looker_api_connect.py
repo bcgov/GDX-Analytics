29271c6aa0582f2358473808783504187964bac7
Veenu Punyani <Veenu.veenu@gov.bc.ca>
2022-01-21 14:31:00 -0800

    pip installer and updated readme

--- maintenance/looker_api/looker_api_connect.py
+++ maintenance/looker_api/looker_api_connect.py
@@ -2,7 +2,7 @@ import looker_sdk
 import os
 import json
 import time
-sdk = looker_sdk.init40("/Users/vveenu/Applications/looker_api/looker.ini")
+sdk = looker_sdk.init40("looker.ini")
 before = time.perf_counter()
 response = sdk.run_query(
     query_id=742831,

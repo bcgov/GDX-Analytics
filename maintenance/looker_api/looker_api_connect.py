7c420ac00075c50ca5f9f52f13e026c9d2642310
Veenu Punyani <Veenu.veenu@gov.bc.ca>
2022-01-21 19:58:34 -0800

    Updated Query to return averages, min, max

--- maintenance/looker_api/looker_api_connect.py
+++ maintenance/looker_api/looker_api_connect.py
@@ -3,19 +3,43 @@ import os
 import json
 import time
 import sys
+import csv
 query = int(sys.argv[1])
+times = int(sys.argv[2])
 
 sdk = looker_sdk.init40("config/looker.ini")
 
-response = sdk.run_query(
+QueryList = []
+
+
+# run query as per user input
+
+i = 1
+while i <= times:
+    response = sdk.run_query(
     query_id=query,
     result_format="json_detail",
     cache=False,
     cache_only=False)
+    # Convert string to Python dict 
+    query_dic = json.loads(response) 
+    Runtime_to_add = round(float(query_dic['runtime']), 2)
+    QueryList.append(Runtime_to_add)
+    i += 1
+    
+    
+print("Querylist", QueryList)
+minimum = min(QueryList)
+print("Min", minimum)
+maximum = max(QueryList)
+print("Max", maximum)
+sum = sum(QueryList)
+length = len(QueryList)
+average = round(sum/length, 2)
+print("Avg", average)
 
-# Convert string to Python dict 
-query_dic = json.loads(response) 
+
+
+
 
     
-print("Looker runtime =", query_dic['runtime']) 
-print("Cache =", query_dic['from_cache']) 

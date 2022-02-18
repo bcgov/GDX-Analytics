3879577be1386bd5aea0daed938982cbb2549761
Lane Clark <52838144+lclarko@users.noreply.github.com>
2022-02-18 13:37:55 -0800

    Update 02-join-external-events.sql

--- maintenance/s3-archive-restore/02-join-external-events.sql
+++ maintenance/s3-archive-restore/02-join-external-events.sql
@@ -1,9 +1,9 @@
--- Build CTE with external events table data
+-- Build CTE with external atomic events data
 WITH he AS (
     SELECT
         ev.event_id,
         ev.collector_tstamp,
-        COALESCE(ev.page_urlhost,'') AS page_urlhost,
+        ev.page_urlhost,
         ev.page_url,
         ev.domain_sessionid
     FROM atomic_spectrum_2020_07.events AS ev
@@ -11,9 +11,11 @@ WITH he AS (
             ON ta.root_id = ev.event_id AND
             ta.root_tstamp = ev.collector_tstamp
         WHERE
+            -- wp.month limits spectrum s3 scan to specified partition 
             ev.month LIKE '2021-09-01 00:00:00' AND
             ta.root_tstamp >= '2021-09-01'
 )
+-- update the base table with historical events data
 UPDATE test.atomic_sept_full_poc_parquet
     SET page_urlhost = he.page_urlhost,
         page_url = he.page_url,

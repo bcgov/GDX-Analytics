436aa1d2a3d02b7a39bc53a134073577ab5f5834
Lane Clark <52838144+lclarko@users.noreply.github.com>
2022-02-18 12:15:59 -0800

    Update 01-populate-base-table.sql

--- maintenance/s3-archive-restore/01-populate-base-table.sql
+++ maintenance/s3-archive-restore/01-populate-base-table.sql
@@ -1,4 +1,4 @@
--- Build a CTE from atomic business area tables
+-- Build CTE from atomic business area tables
 INSERT INTO test.atomic_sept_full_poc_parquet
 
 WITH custom_events AS
@@ -27,10 +27,13 @@ SELECT DISTINCT
     wp.id AS page_view_id,
     wp.root_id
 FROM custom_events AS ce
+-- join external_schema.glue_data_catalog_table
 JOIN atomic_spectrum_2020_07.snowplow_web_page_1 AS wp
     ON ce.root_id = wp.root_id AND ce.root_tstamp = wp.root_tstamp
-WHERE 
+WHERE
+    -- wp.month limits spectrum s3 scan to specified partition 
     wp.month LIKE '2021-09-01 00:00:00' AND
+    -- anecdotal performance gain from timestamp condition
     wp.root_tstamp >= '2021-09-01';
 
-GRANT SELECT ON test.atomic_sept_full_poc_parquet to looker;
+GRANT SELECT ON test.atomic_sept_full_poc_parquet TO looker;

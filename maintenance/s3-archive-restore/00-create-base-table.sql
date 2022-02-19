88a2755694663653928b4d13890389441d63c305
Lane Clark <52838144+lclarko@users.noreply.github.com>
2022-02-18 16:33:19 -0800

    Update 00-create-base-table.sql

--- maintenance/s3-archive-restore/00-create-base-table.sql
+++ maintenance/s3-archive-restore/00-create-base-table.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS test.atomic_sept_full_poc_parquet (
     domain_sessionid    CHAR(128)       ENCODE ZSTD,
     PRIMARY KEY(root_id)
 )
+-- Set diststyle to key to avoid
 DISTSTYLE KEY
 -- Optimize join to events table
 DISTKEY (root_id)

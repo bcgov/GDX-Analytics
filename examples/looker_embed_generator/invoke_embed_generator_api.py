ca88a40e4393ca6050c73acd2bb776e556bc3527
BJenning <brendan.jennings@gov.bc.ca>
2022-03-23 11:25:33 -0700

    Pointed example to custom domain endpoint.

--- examples/looker_embed_generator/invoke_embed_generator_api.py
+++ examples/looker_embed_generator/invoke_embed_generator_api.py
@@ -19,15 +19,13 @@
 import requests
 import os
 import json
-import boto3
 from aws_requests_auth.aws_auth import AWSRequestsAuth
 
 api_secret_access_key=os.environ['API_SECRET_ACCESS_KEY']
 
 api_id='jdo0epycg8'
 region='ca-central-1'
-api_stage='default'
-endpoint=f'https://{api_id}.execute-api.{region}.amazonaws.com/{api_stage}/'
+endpoint=f'https://api.analytics.gov.bc.ca/embed/'
 params="dashboard_id=120&embed_domain=localhost:8888&urlhost=www2.gov.bc.ca"
 headers={"x-api-key": api_secret_access_key}
 

18775b01c7c850dd1718d705c4a0f2ca2235db1a
BJenning <brendan.jennings@gov.bc.ca>
2022-03-23 10:21:28 -0700

    Finalized example invoke script.

--- examples/looker_embed_generator/invoke_embed_generator_api.py
+++ examples/looker_embed_generator/invoke_embed_generator_api.py
@@ -4,29 +4,20 @@ import json
 import boto3
 from aws_requests_auth.aws_auth import AWSRequestsAuth
 
-aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID']
-aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+api_secret_access_key=os.environ['API_SECRET_ACCESS_KEY']
 
 api_id='jdo0epycg8'
 region='ca-central-1'
 api_stage='default'
 endpoint=f'https://{api_id}.execute-api.{region}.amazonaws.com/{api_stage}/'
-headers={"params":"dashboard_id=120&urlhost=www2.gov.bc.ca"}
-
-session = boto3.Session(aws_access_key_id=aws_access_key_id,
-                        aws_secret_access_key=aws_secret_access_key,
-                        )
-credentials = session.get_credentials()
-
-auth = AWSRequestsAuth(aws_access_key=credentials.access_key,
-                       aws_secret_access_key=credentials.secret_key,
-                       aws_host=f'{api_id}.execute-api.{region}.amazonaws.com',
-                       aws_region=region,
-                       aws_service='execute-api')
+params="dashboard_id=120&embed_domain=localhost:8888&urlhost=www2.gov.bc.ca"
+headers={"x-api-key": api_secret_access_key}
 
 response = requests.get(endpoint,
-                        auth=auth, 
+                        params=params,
                         headers=headers)
 
-print(json.loads(response.content.decode('utf-8'))['body'])
-
+if 'body' in json.loads(response.content.decode('utf-8')):
+    print(json.loads(response.content.decode('utf-8'))['body'])
+else:
+    print(response.content.decode('utf-8'))

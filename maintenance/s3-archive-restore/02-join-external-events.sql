b106e1da0c49a28c9a93a28d9ad40560971e5d01
Lane Clark <52838144+lclarko@users.noreply.github.com>
2022-03-24 14:53:18 -0700

    Update 02-join-external-events.sql

--- maintenance/s3-archive-restore/02-join-external-events.sql
+++ maintenance/s3-archive-restore/02-join-external-events.sql
@@ -1,25 +1,13 @@
--- Build CTE with external atomic events data
-WITH he AS (
-    SELECT
-        ev.event_id,
-        ev.collector_tstamp,
-        ev.page_urlhost,
-        ev.page_url,
-        ev.domain_sessionid
-    FROM atomic_spectrum_2020_07.events AS ev
-        INNER JOIN test.atomic_sept_full_poc_parquet AS ta
-            ON ta.root_id = ev.event_id AND
-            ta.root_tstamp = ev.collector_tstamp
-        WHERE
-            -- wp.month limits spectrum s3 scan to specified partition 
-            ev.month LIKE '2021-09-01 00:00:00' AND
-            ta.root_tstamp >= '2021-09-01'
-)
--- update the base table with historical events data
-UPDATE test.atomic_sept_full_poc_parquet
-    SET page_urlhost = he.page_urlhost,
-        page_url = he.page_url,
-        domain_sessionid = he.domain_sessionid
-FROM he
-    WHERE atomic_sept_full_poc_parquet.root_id = he.event_id AND
-    atomic_sept_full_poc_parquet.root_tstamp = he.collector_tstamp;
+-- Update on cluster history table with
+-- archived external atomic events data
+UPDATE test.atomic_sept_poc_parquet
+    SET page_urlhost = ev.page_urlhost,
+        page_url = ev.page_url,
+        domain_sessionid = ev.domain_sessionid
+FROM atomic_spectrum_2020_07.events AS ev
+    WHERE
+        -- spectrum partition filter month and app_id
+        ev.month = '2021-09-01 00:00:00' AND
+        ev.app_id = 'Snowplow_gov' AND
+        atomic_sept_poc_parquet.root_id = ev.event_id AND
+        atomic_sept_poc_parquet.root_tstamp = ev.collector_tstamp;

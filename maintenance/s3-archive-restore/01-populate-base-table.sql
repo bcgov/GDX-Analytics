0fbd4121ba37131160e366e292bab6ed6d864efe
Lane Clark <lane.clark@gov.bc.ca>
2022-04-11 15:05:34 -0700

    add addition context tables

--- maintenance/s3-archive-restore/01-populate-base-table.sql
+++ maintenance/s3-archive-restore/01-populate-base-table.sql
@@ -1,11 +1,25 @@
 -- Build CTE from atomic business area tables
-INSERT INTO test.atomic_sept_full_poc_parquet
+INSERT INTO test.atomic_sept_poc_parquet
 
-WITH custom_events AS
+WITH custom_context_events AS
 (
     SELECT root_id, root_tstamp FROM atomic.ca_bc_gov_workbc_find_career_1 WHERE root_tstamp >= '2021-09-01' AND root_tstamp < '2021-10-01'
 UNION
-    SELECT root_id, root_tstamp FROM atomic.ca_bc_gov_cfmspoc_appointment_step_1 WHERE root_tstamp >= '2021-09-01' AND root_tstamp < '2021-10-01'
+    SELECT root_id, root_tstamp FROM atomic.bc_gov_workbc_resource_click_1 WHERE root_tstamp >= '2021-09-01' AND root_tstamp < '2021-10-01'
+UNION
+    SELECT root_id, root_tstamp FROM atomic.bc_gov_workbc_find_resources_1 WHERE root_tstamp >= '2021-09-01' AND root_tstamp < '2021-10-01'
+UNION
+    SELECT root_id, root_tstamp FROM atomic.bc_gov_wellbeing_wellbeing_click_1 WHERE root_tstamp >= '2021-09-01' AND root_tstamp < '2021-10-01'
+UNION
+    SELECT root_id, root_tstamp FROM atomic.ca_bc_gov_wellbeing_wellbeing_resources_1 WHERE root_tstamp >= '2021-09-01' AND root_tstamp < '2021-10-01'
+UNION
+    SELECT root_id, root_tstamp FROM atomic.ca_bc_gov_wellbeing_wellbeing_wordcloud_1 WHERE root_tstamp >= '2021-09-01' AND root_tstamp < '2021-10-01'
+UNION
+    SELECT root_id, root_tstamp FROM atomic.ca_bc_gov_bcs_calendar_click_1 WHERE root_tstamp >= '2021-09-01' AND root_tstamp < '2021-10-01'
+UNION
+    SELECT root_id, root_tstamp FROM atomic.ca_bc_gov_googtrans_google_translate_1 WHERE root_tstamp >= '2021-09-01' AND root_tstamp < '2021-10-01'
+UNION
+    SELECT root_id, root_tstamp FROM atomic.ca_bc_gov_gateway_action_1 WHERE root_tstamp >= '2021-09-01' AND root_tstamp < '2021-10-01'
 UNION
     SELECT root_id, root_tstamp FROM atomic.ca_bc_gov_ldb_click_1 WHERE root_tstamp >= '2021-09-01' AND root_tstamp < '2021-10-01'
 UNION
@@ -26,7 +40,7 @@ SELECT
     wp.root_tstamp,
     wp.id AS page_view_id,
     wp.root_id
-FROM custom_events AS ce
+FROM custom_context_events AS ce
 -- join external_schema.glue_data_catalog_table
 JOIN atomic_spectrum_2020_07.snowplow_web_page_1 AS wp
     ON ce.root_id = wp.root_id AND ce.root_tstamp = wp.root_tstamp
@@ -40,4 +54,4 @@ GROUP BY
     wp.id,
     wp.root_id;
 
-GRANT SELECT ON test.atomic_sept_full_poc_parquet TO looker;
+GRANT SELECT ON test.atomic_sept_poc_parquet TO looker;

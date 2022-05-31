67950f0e71c42fefcb6b62bc1d7d14f9e912ecdb
Veenu Punyani <Veenu.veenu@gov.bc.ca>
2022-05-30 17:14:16 -0700

    removed unused import

--- examples/react_tracker/src/index.js
+++ examples/react_tracker/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import { Route, Routes, Link, BrowserRouter as Router, BrowserRouter } from 'react-router-dom'
+import { Route, Routes, Link, BrowserRouter } from 'react-router-dom'
 import App from './App'
 import SecondPage from './SecondPage'
 import Thirdpage from './ThirdPage'

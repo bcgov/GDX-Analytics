b9007357b7f6d9573dab1c2d3adf9daf9560c433
Ozdemir Ozcelik <ozdemir.ozcelik@gov.bc.ca>
2024-09-27 10:57:45 -0700

    added test script

---
+++ maintenance/TEST-GDXDSD-7198.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+##################################################
+# THIS IS A TEST SCRIPT TO USE FOR GDXDSD-7198
+##################################################
+
+# echo "TEST: JUST PRINTING getRsTableSize FUNCTIONS:"
+# echo "TEST: Construction SQL query ..."
+# echo "TEST: Output log file and clean ..."
+# echo "TEST: Upload logs to S3 Clint..."
+# echo "TEST: Import from S3 to Redshift ..."
+# echo "TEST: Move files to S3 Good ..."
+# echo "TEST: Delete >7 days old logs from EC2 ..."
+echo "TEST: INFO:  Load into table 'table_sizes' completed, ### record(s) loaded successfully ..."
+
+
+

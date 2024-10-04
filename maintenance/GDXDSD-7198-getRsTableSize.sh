dd7465e48343ec7364e8b9a05837f01d5c3dd76b
Ozdemir Ozcelik <ozdemir.ozcelik@gov.bc.ca>
2024-10-04 15:59:50 -0700

    testing log formatting -insert errro to task -16

--- maintenance/GDXDSD-7198-getRsTableSize.sh
+++ maintenance/GDXDSD-7198-getRsTableSize.sh
@@ -170,10 +170,9 @@ return 0
 # and redirect subshell's stderr to parent's stderr
 error_message=$( (
     set -e
-    run_table_size_task
-) 2>&1 ) | process_task_output
+    run_table_size_task | process_task_output
+) 2>&1 )
 status=${PIPESTATUS[0]}
-
 
 echo "Exit status of the task: $status"
 echo "$error_message"

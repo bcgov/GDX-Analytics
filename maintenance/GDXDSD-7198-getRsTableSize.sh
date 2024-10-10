939aebc49fb07fb5c03f04e669c8dbb34e14a17d
Ozdemir Ozcelik <ozdemir.ozcelik@gov.bc.ca>
2024-10-10 10:53:26 -0700

    email changes

--- maintenance/GDXDSD-7198-getRsTableSize.sh
+++ maintenance/GDXDSD-7198-getRsTableSize.sh
@@ -131,11 +131,16 @@ find $LOG_PATH -mindepth 1 -mtime +7 -delete
 # Check the report interval and send the log report, and delete logs for >7 days
 if [ "$FORCE_REPORT" = true ] || ([ $((HOUR % REPORT_INTERVAL_HOURS)) -eq 0 ] && [ "$MINUTE" -eq 00 ]); then
     if [ -s $REPORT_LOG_FILE ]; then
-        # Send an email with the log content
-        echo "Sending report email at $CURRENT_TIME"
+        # Echo log content
+        # echo "Sending report email at $CURRENT_TIME"
         # use below to send as text
-        cat $REPORT_LOG_FILE | mail -s "$REPORT_SUBJECT" $REPORT_EMAIL_TO
-        echo "Email sent!"     
+        # echo $REPORT_LOG_FILE
+        # echo "Email sent!"     
+        SUBJECT="Report Email"
+        BODY=$(echo "Sending report email at $CURRENT_TIME"; echo "$REPORT_LOG_FILE"; echo "Email sent!")
+
+        echo "Subject: $SUBJECT\n\n$BODY"
+
     else
         echo "Log file is empty at $CURRENT_TIME, nothing to report." >> $REPORT_LOG_FILE
     fi

b05295bd3096996867fb301738ee7f01880d942f
Ozdemir Ozcelik <ozdemir.ozcelik@gov.bc.ca>
2024-11-06 14:20:06 -0800

    centralized router configuration

--- examples/vue_js_tracker/src/main.js
+++ examples/vue_js_tracker/src/main.js
@@ -1,18 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from './views/HomePage.vue'
-import About from './views/AboutPage.vue'
-
-const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About }
-]
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
 const app = createApp(App)
 
@@ -23,9 +11,8 @@ if (process.env.NODE_ENV === 'production') {
     app.config.productionSourceMap = false
   }
 
-// This is a global After-Hook which calls the Snowplow page tracking event. See the
-// Vue.js documentation here:
-// https://router.vuejs.org/guide/advanced/navigation-guards.html#global-after-hooks
+// This is a global After-Hook which calls the Snowplow page tracking event.
+// Vue.js documentation here: https://router.vuejs.org/guide/advanced/navigation-guards.html#global-after-hooks
 router.afterEach(() => {
     window.snowplow('trackPageView');
   })

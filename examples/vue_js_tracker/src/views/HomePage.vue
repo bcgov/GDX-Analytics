d38f34dc0835a6ce3b7876eecc811ce5efa718b7
Ozdemir Ozcelik <ozdemir.ozcelik@gov.bc.ca>
2024-11-06 15:38:14 -0800

    typo fix

--- examples/vue_js_tracker/src/views/HomePage.vue
+++ examples/vue_js_tracker/src/views/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="home">
       <img alt="Vue logo" src="../assets/logo.png" />
-      <h1>Welcome to Vue 3.js Snowplow Tracker Test Page!</h1>
+      <h1>Welcome to Vue.js Snowplow Tracker Test Page!</h1>
       This is an example of a two page Vue.js app. It has the Snowplow Javascript Standalone web tracker vE.2.14.0 Installed.
       Click the links to activate more Snowplow events.
     </div>

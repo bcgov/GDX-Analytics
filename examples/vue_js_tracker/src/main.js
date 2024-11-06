3024c422d893c67ff431c6adde61276c547a14d6
Ozdemir Ozcelik <ozdemir.ozcelik@gov.bc.ca>
2024-11-06 14:11:32 -0800

    migration to Vue 3 -completed

--- examples/vue_js_tracker/src/main.js
+++ examples/vue_js_tracker/src/main.js
@@ -1,17 +1,35 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './views/HomePage.vue'
+import About from './views/AboutPage.vue'
 
-Vue.config.productionTip = false
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About }
+]
 
-new Vue({
-  router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+// Disable production source maps
+if (process.env.NODE_ENV === 'production') {
+    app.config.productionSourceMap = false
+  }
 
 // This is a global After-Hook which calls the Snowplow page tracking event. See the
 // Vue.js documentation here:
 // https://router.vuejs.org/guide/advanced/navigation-guards.html#global-after-hooks
-router.afterEach((to, from) => {
-  window.snowplow('trackPageView');
-})
+router.afterEach(() => {
+    window.snowplow('trackPageView');
+  })
+
+app.mount('#app')
+
+
---
+++ examples/vue_js_tracker/src/views/HomePage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="home">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <h1>Welcome to Vue 3.js Snowplow Tracker Test Page!</h1>
+      This is an example of a two page Vue.js app. It has the Snowplow Javascript Standalone web tracker vE.2.14.0 Installed.
+      Click the links to activate more Snowplow events.
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage'
+  }
+  </script>
+  
+  <style scoped>
+  .home {
+    text-align: center;
+  }
+  
+  img {
+    width: 200px;
+  }
+  </style>

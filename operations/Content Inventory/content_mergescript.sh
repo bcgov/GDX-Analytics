7ff3444d47ba302383d1c09978c90a49ad0aee82
bradleyoldham <117430266+bradleyoldham@users.noreply.github.com>
2025-04-04 16:17:50 -0700

    Updates from Review conversation to remove the prompted inputs and only support 3 input variable operation

--- operations/Content Inventory/content_mergescript.sh
+++ operations/Content Inventory/content_mergescript.sh
@@ -1,49 +1,29 @@
 #!/bin/bash
 # merge Client Inventory Analysis for Content CSV files
-# Expecting no input variables or three input varaibles in the order
+# Expecting three input varaibles in the order
 # 1st parameter is the Explore CSV
 # 2nd parameter is the SQL CSV
 # 3rd parameter is the Output CSV
 
+if [ $# -eq 0 ] # if there are no input variables
+  then
+    printf "This script is expecting three input variables, not one\nAborting\n"
+    exit 1 # abort the script
+fi
+
 if [ $# -eq 1 ] # if there is only one input variable
   then
-    printf "This script is expecting zero or three input variables, not one\nAborting\n"
+    printf "This script is expecting three input variables, not one\nAborting\n"
     exit 1 # abort the script
 fi
 
 if [ $# -eq 2 ] # if there is only two input variables
   then
-    printf "This script is expecting zero or three input variables, not two\nAborting\n"
+    printf "This script is expecting three input variables, not two\nAborting\n"
     exit 1 # abort the script
 fi
 
-if [ $# -eq 0 ]
-     then
-
-        read -p 'Page Explore CSV: ' explore_var
-        if ! test -f $explore_var; then #check if the provided file exists
-            printf "ERROR: The file \"$explore_var\" does not exist\nAborting\n"
-            exit 22 # abort the script
-        fi
-
-        read -p 'Page SQL CSV: ' sql_var
-        if ! test -f $sql_var; then #check if the provided file exists
-            printf "ERROR: The file \"$sql_var\" does not exist\nAborting\n"
-            exit 22 # abort the script
-        fi
-
-        read -p 'Output CSV: ' output_var
-        if  test -f $output_var; then #check if the provided DOES NOT file exists
-            printf "ERROR: The file \"$output_var\" already exists\nAborting\n"
-            exit 22 # abort the script
-        fi
-        # line below is the merge command based on the first column of Node ID
-        # merge takes the Meta Data and merges on the Page Views
-        # there is a temp file "temp.csv" that is created and destoryed
-        (sed 's/Page View Start Month/metadata.node_id/g' $explore_var |grep -v 'Node ID' > temp.csv ) && join -j1 -t,  $sql_var temp.csv   > $output_var   && join -j1 -t, -v 1 $sql_var temp.csv >> $output_var && rm temp.csv
-fi
-
-# The same as above but with the file names in the order EXPLORE SQL OUTOUT
+# The operational code for merging files
 
 if [ $# -eq 3 ]
   then

bd777be3356f26070a5b142ff5ce125e627cb6da
Ozdemir Ozcelik <ozdemir.ozcelik@gov.bc.ca>
2025-08-29 15:24:49 -0700

    Updated comments

--- examples/vue_js_tracker/vite.config.js
+++ examples/vue_js_tracker/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 8080
+    port: 8080 // uses the default 5173 if not set
   },
   build: {
     sourcemap: false // disables source maps for production
